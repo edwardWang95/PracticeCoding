b2314c5931951bfa8f4052611e245c2bcbcef09c
edwardWang95 <edward.wang.95@gmail.com>
2017-10-18 12:47:19 -0400

    Temp

---
+++ src/LeetCode/BattleShipsInABoard.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+/**
+ * Created by edwardwang on 2/7/17.
+ */
+class BattleShipsInABoardTest{
+    public static void main(String[] args){
+        char[][] example = {{'X','.','.','X'},
+                            {'.','.','.','X'},
+                            {'.','.','.','X'},
+                            {'.','.','.','X'}};
+
+    }
+
+}
+public class BattleShipsInABoard {
+    public int countBattleships(char[][] board) {
+        int count = 0;
+
+
+
+        return count;
+    }
+
+
+
+
+}
