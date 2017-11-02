df3450de51f7c1d77bb6f19c238967c96b521c3f
Edward Wang <edward.wang.95@gmail.com>
2017-11-02 12:37:28 -0400

    Daily HW Problem: Multiset backtracking permutation

--- src/Sandbox/SandboxMain.java
+++ src/Sandbox/SandboxMain.java
@@ -5,7 +5,18 @@ package Sandbox;
  */
 public class SandboxMain {
     public static void main(String[] args){
+        multisetsTest();
+    }
+
+    static void derangementTest(){
         Derangement derangement = new Derangement();
         derangement.derangement(3);
+    }
+
+    static void multisetsTest(){
+        int test[] = {1,1,2,2};
+        int NMax = 6;
+        Multisets multisets = new Multisets(test,NMax);
+        multisets.printPermutations();
     }
 }
