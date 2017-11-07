a406bfbf8d9f3bc4075b07a93f6e3bc6fccb88f1
Edward Wang <edward.wang.95@gmail.com>
2017-11-07 11:48:00 -0500

    Dynamic Programming

--- src/Sandbox/SandboxMain.java
+++ src/Sandbox/SandboxMain.java
@@ -5,7 +5,7 @@ package Sandbox;
  */
 public class SandboxMain {
     public static void main(String[] args){
-        multisetsTest();
+        dynamicProgrammingTest();
     }
 
     static void derangementTest(){
@@ -18,5 +18,23 @@ public class SandboxMain {
         int NMax = 6;
         Multisets multisets = new Multisets(test,NMax);
         multisets.printPermutations();
+    }
+
+    static void dynamicProgrammingTest(){
+        String X = "chocolate";
+        String Y = "chips";
+        String Z1 = "cchocohilaptes";
+        String Z2 = "chocochilatspe";
+        DyanmicProgramming dyanmicProgramming = new DyanmicProgramming();
+        System.out.println("Normal Programming");
+        dyanmicProgramming.setXYZ(X,Y,Z1);
+        System.out.println("Z1: " + dyanmicProgramming.isZCorrectNormal());
+        dyanmicProgramming.setXYZ(X,Y,Z2);
+        System.out.println("Z2: " + dyanmicProgramming.isZCorrectNormal());
+        System.out.println("Dynamic Programming");
+        dyanmicProgramming.setXYZ(X,Y,Z1);
+        System.out.println("Z1: " + dyanmicProgramming.isZCorrectDynamicProgramming());
+        dyanmicProgramming.setXYZ(X,Y,Z2);
+        System.out.println("Z2: " + dyanmicProgramming.isZCorrectDynamicProgramming());
     }
 }
