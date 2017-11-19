ed70d41d64d1911f742fbba3ec194da048775968
Edward Wang <edward.wang.95@gmail.com>
2017-11-18 21:43:47 -0500

    All permutations

--- src/Sandbox/SandboxMain.java
+++ src/Sandbox/SandboxMain.java
@@ -5,7 +5,7 @@ package Sandbox;
  */
 public class SandboxMain {
     public static void main(String[] args){
-        multisetsTest();
+        backtrackingSubsets();
     }
 
     static void derangementTest(){
@@ -18,5 +18,12 @@ public class SandboxMain {
         int NMax = 6;
         Multisets multisets = new Multisets(test,NMax);
         multisets.printPermutations();
+    }
+
+    static void backtrackingSubsets(){
+        int k = 3;  //elements in a subset
+        int n = 5;  //total numbers
+        BacktrackingSubsets backtrackingSubsets = new BacktrackingSubsets(n,k);
+        backtrackingSubsets.backtracking();
     }
 }
