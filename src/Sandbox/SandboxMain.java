04cd9141fa4f1457f2f61e01a270228a9775ce4c
edwardWang95 <edward.wang.95@gmail.com>
2017-12-07 02:53:59 -0500

    Added traceback to editstring to find best string match

--- src/Sandbox/SandboxMain.java
+++ src/Sandbox/SandboxMain.java
@@ -21,9 +21,22 @@ public class SandboxMain {
     }
 
     static void editStrings(){
-        String m = "watch the movie raising arizona?";
-        String n = "watch da mets raze arizona?";
-        EditStrings editStrings = new EditStrings(m,n);
+        String m = " thou-shalt-not";
+        String n = " you-should-not";
+
+        String x = " ababa";
+        String y = " bzbzb";
+
+        String test1a = " watch the movie raising arizona?";
+        String test1b = " watch da mets raze arizona?";
+
+        String test2a = " this is what happens when I type slow";
+        String test2b = " htishisth whaty havpens when ui type fasht";
+
+        String test3a = " leonard skiena";
+        String test3b = " lynard skynard";
+
+        EditStrings editStrings = new EditStrings(test3a,test3b);
         editStrings.dynamicProgramming();
     }
 }
