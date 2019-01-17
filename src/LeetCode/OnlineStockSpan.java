b0f09f79ef35a97f58a64a3cdc1631957c269519
Edward Wang <edward.wang.95@gmail.com>
2019-01-16 21:24:31 -0500

    Got correct result for online stock spanner test 1.

--- src/LeetCode/OnlineStockSpan.java
+++ src/LeetCode/OnlineStockSpan.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.ArrayList;
+
 /**
  * 901. Online Stock Span
  * Write a class StockSpanner which collects daily price quotes for some stock, and
@@ -13,4 +15,109 @@ package LeetCode;
  * then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
  * */
 public class OnlineStockSpan {
+    public static void main(String[] args)
+    {
+        System.out.println("Online Stock Span");
+        test1();
+    }
+    /**
+     * Your StockSpanner object will be instantiated and called as such:
+     * StockSpanner obj = new StockSpanner();
+     * int param_1 = obj.next(price);
+     *
+     * To have a null return within an array, rather than an int primitive variable
+     * use the Integer object that wraps around the int variable.
+     */
+    private static void test1()
+    {
+        System.out.println("Online Stock Spanner Test 1:\t");
+        StockSpanner obj = new StockSpanner();
+        String[] inputString = {"StockSpanner","next","next","next","next","next","next","next"};
+        Integer[] input = {null, 100, 80, 60, 70, 60, 75, 85};
+        Integer[] expectedOutput = {null, 1, 1, 1, 2, 1, 4, 6};
+        ArrayList<Integer> output = new ArrayList<>();
+        for(int i=0;i<inputString.length;i++)
+        {
+            if(inputString[i].equals("StockSpanner"))output.add(null);
+            else if (inputString[i].equals("next"))
+            {
+                output.add(obj.next(input[i]));
+                //System.out.println("Price: "+ input[i]+"\tOutput: "+output.get(i) + "\tExpected: "+expectedOutput[i]);
+                if(!output.get(i).equals(expectedOutput[i])) break;
+                //System.out.println();
+            }
+        }
+        System.out.println(output.size() != input.length ? "Incorrect" : "Correct");
+    }
+}
+
+/**
+ * This challenge has a time limit, so it's focus will be on speed.
+ * */
+class StockSpanner
+{
+    ArrayList<Integer> prices = new ArrayList<Integer>();
+
+    public StockSpanner()
+    {
+
+    }
+
+    /**
+     * Every price starts with a value of 1.
+     * Search all previous stock values to and increment the count if they are less than current price.
+     *
+     * Array list so I can dynamically increase the size, merge sort into the array and then update the
+     * counter based on it's location.
+     * bucket sort based on tens values into insertion sort
+     * */
+    public int next(int price)
+    {
+        if(price < 0 || price > 100000) return 0;
+        return getSortedIndex(price);
+        /*int temp = getSortedIndex(price);
+        printArrayList();
+        return temp;
+        */
+    }
+
+    private void printArrayList()
+    {
+        System.out.println("Print Array List");
+        for(int i: prices) System.out.print(i+", ");
+        System.out.println();
+    }
+
+    private int getSortedIndex(int val)
+    {
+        //base case if empty
+        if(prices.size() == 0)
+        {
+            prices.add(val);
+        }else
+        {
+            return binaryInsertionSort(val, prices.size()/2, -1);
+        }
+        return 1;
+    }
+
+    //O(log(n)) search for location to insert
+    private int binaryInsertionSort(int val, int middle, int previousMiddle)
+    {
+        if(middle == previousMiddle)
+        {
+            //minute shifts after getting closest to middle value
+            if(prices.get(previousMiddle) < val) middle++;
+            else if(prices.get(previousMiddle) > val && middle != 0 && prices.size()%2==0)middle--;    //if odd amount of values then decrement
+        }else if(middle == prices.size()){
+            middle = prices.size()-1;
+        }else {
+            //O(log(n)) shifting to closest insertion location
+            if (prices.get(middle) > val) return binaryInsertionSort(val, middle - (middle - previousMiddle)/2, middle);
+            else if (prices.get(middle) < val) return binaryInsertionSort(val, middle + middle/2, middle);
+        }
+        //add in value and return that plus 1 for the value itself
+        prices.add(middle, val);
+        return middle+1;
+    }
 }
