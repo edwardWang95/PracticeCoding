cc05d6264df020486e398322c57edcb1a4af9b6c
Edward Wang <edward.wang.95@gmail.com>
2019-01-23 22:04:14 -0500

    18. 4Sum

---
+++ src/LeetCode/FourSum.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+import java.util.List;
+
+/**
+ * 18. 4Sum
+ *
+ * Given an array nums of n integers and an integer target, are there elements
+ * a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets
+ * in the array which gives the sum of target.
+ *
+ * Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+ *
+ * A solution set is:
+ * [
+ *   [-1,  0, 0, 1],
+ *   [-2, -1, 1, 2],
+ *   [-2,  0, 0, 2]
+ * ]
+ * */
+public class FourSum {
+    public static void main(String[] args)
+    {
+        FourSum solution = new FourSum();
+    }
+
+    private static void test1(FourSum solution)
+    {
+
+    }
+}
+
+class FourSumSolution
+{
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        return null;
+    }
+}
