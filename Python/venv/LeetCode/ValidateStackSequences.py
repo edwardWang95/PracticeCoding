8fe4aec8d0533e391ab7771b201addae5301a773
Edward Wang <edward.wang.95@gmail.com>
2019-02-24 20:52:06 -0500

    Begin coding

--- Python/venv/LeetCode/ValidateStackSequences.py
+++ Python/venv/LeetCode/ValidateStackSequences.py
@@ -5,13 +5,24 @@ return true if and only if this could have been the result of a sequence of
 push and pop operations on an initially empty stack.
 """
 
+class ValidateStackSequencesSolution:
+    def validateStackSequences(self, pushed, popped):
+
+
 class ValidateStackSequences:
-    def temp(self):
-        print("Temp")
+    def createTest(self):
+
+    def test1(self):
+        pushed = [1,2,3,4,5]
+        popped = [4,5,3,2,1]
+        expectedOutput = True
+
+    def runtTests(self):
+        self.test1()
 
     def __init__(self):
         print("Validate Stack Sequences")
-        self.temp()
+        self.runtTests()
 
 
 if __name__=="__main__":
