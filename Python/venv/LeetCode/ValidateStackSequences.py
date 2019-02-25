edec03a2a08d32d7cdb4d2857ccfcb0d86045055
Edward Wang <edward.wang.95@gmail.com>
2019-02-24 20:33:15 -0500

    Began setting up Python environment to work with

---
+++ Python/venv/LeetCode/ValidateStackSequences.py
@@ -0,0 +1,18 @@
+"""
+946. Validate Stack Sequences
+Given two sequences pushed and popped with distinct values,
+return true if and only if this could have been the result of a sequence of
+push and pop operations on an initially empty stack.
+"""
+
+class ValidateStackSequences:
+    def temp(self):
+        print("Temp")
+
+    def __init__(self):
+        print("Validate Stack Sequences")
+        self.temp()
+
+
+if __name__=="__main__":
+    validateStackSequences = ValidateStackSequences()
