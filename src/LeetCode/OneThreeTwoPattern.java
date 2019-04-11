6f1c54535f6d32d7d524f2d8552d84616d7ccacc
Edward Wang <edward.wang,95@gmail.com>
2019-04-11 15:10:59 -0400

    Setup dynamic programming. Failing second test case

--- src/LeetCode/OneThreeTwoPattern.java
+++ src/LeetCode/OneThreeTwoPattern.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Leetcode 456. 132 Pattern
+ *
  * Given a sequence of n integers a1, a2, ..., an, a 132 pattern is a subsequence ai, aj, ak such that
  * i < j < k and ai < ak < aj. Design an algorithm that takes a list of n numbers as input and checks whether
  * there is a 132 pattern in the list.
@@ -26,20 +28,59 @@ import java.util.List;
  * Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
  * */
 public class OneThreeTwoPattern {
+    public static void main(String[] args){
+        System.out.println("Leetcode 456. 132 Pattern");
+        test1();
+        test2();
+    }
 
+    private static void test1(){
+        int [] nums = {1,2,3,4};
+        boolean expected = false;
+        printTestAndResult(1, nums, expected);
+    }
+
+    private static void test2(){
+        int [] nums = {3, 1, 4, 2};
+        boolean expected = true;
+        printTestAndResult(2, nums, expected);
+    }
+
+    private static void printTestAndResult(int testNum, int[] nums, boolean expected){
+        OneThreeTwoPatternSolution solution = new OneThreeTwoPatternSolution();
+        boolean actual = solution.find132pattern(nums);
+        System.out.println("Test: "+testNum);
+        System.out.print("Input:\t");
+        for(int i:nums) System.out.print(i+"\t");
+        System.out.print("\n");
+        System.out.println("Expected: " + expected);
+        System.out.println("Actual: " + actual);
+        System.out.println(expected==actual ? "Passed\n" : "Failed\n");
+    }
 }
 
 class OneThreeTwoPatternSolution{
     public boolean find132pattern(int[] nums) {
-        return find132pattern(nums, 0, 0, 0);
+        return find132pattern(nums, 0, 1, 2);
     }
 
     public boolean find132pattern(int[] nums, int i, int j, int k) {
         if(isSolution(nums, i, j, k)){
             return true;
         }else{
-            List<Integer> jList = jCandidates();
-            List<Integer> kList = kCandidates();
+            for(;i<nums.length;i++){
+                List<Integer> jList = jCandidates(nums, i, j, k);
+                while(jList.size() != 0){
+                    j = jList.get(0);
+                    List<Integer> kList = kCandidates(nums, i, j, k);
+                    while(kList.size() != 0) {
+                        k = kList.get(0);
+                        if(find132pattern(nums, i, j, k)) return true;
+                        kList.remove(0);
+                    }
+                    jList.remove(0);
+                }
+            }
         }
         return false;
     }
@@ -50,7 +91,7 @@ class OneThreeTwoPatternSolution{
 
     private List<Integer> jCandidates(int[] nums, int i, int j, int k){
         List<Integer> candidates = new ArrayList<>();
-        
+
         return candidates;
     }
 
